6638b6363aecea900a90494701d465af1e663245
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-27 22:58:47 +0100

    refactor: Refactor list of tolls

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
     <div class="flex flex-wrap justify-around p-4 w-full">
-        <div id="tolls" class="flex flex-col bg-[#434141] rounded-lg text-white font-bold w-[17rem] overflow-hidden gap-2">
+        <div id="tolls"
+            class="flex flex-col bg-[#434141] rounded-lg text-white font-bold w-[17rem] overflow-hidden gap-2">
             <div class="flex text-2xl justify-center pt-1 px-1">Tolls</div>
             <div class="text-lg">
                 @forelse ($tolls as $toll)
-                    <a class="flex justify-around gap-1 min-w-56 border-t-[1px] p-1.5 cursor-pointer hover:bg-[#5b5858]" href="{{-- {{ route('showToll', $toll->id) }} --}}">
+                    <a class="flex justify-around gap-1 min-w-56 border-t-[1px] p-1.5 cursor-pointer hover:bg-[#5b5858]"
+                        href="{{-- {{ route('showToll', $toll->id) }} --}}">
                         <div>
                             {{ $toll->name }}
                         </div>
                         <div class="font-normal">
-                            {{ $toll->station_value}} €
+                            {{ $toll->station_value }} €
                         </div>
                     </a>
                 @empty
@@ -21,4 +23,5 @@
                 @endforelse
             </div>
         </div>
+    </div>
 @endsection
