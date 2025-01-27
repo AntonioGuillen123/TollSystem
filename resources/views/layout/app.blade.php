16fe62012480c8f71aafaed56b75d700fba96f42
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-27 21:05:28 +0100

    feat: Create app layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Toll System')</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div id="app" class="flex flex-col justify-between min-h-screen">
+        <x-header></x-header>
+
+        <main class="flex justify-center">
+            @yield('content')
+        </main>
+
+        <x-footer></x-footer>
+    </div>
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
