8eb25aa87e0cf6ed618c46e1d2247bffa8783d3c
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-27 15:59:19 +0100

    feat: Create route for web list of tolls

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TollController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/toll', [TollController::class, '__invoke'])->name('listOfTolls');
