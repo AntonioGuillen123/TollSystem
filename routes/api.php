47a6f2422acc2baa34edfda3252ac899795ff62e
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-27 15:47:04 +0100

    feat: Create route for API create toll tickets

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\TollController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/tollTicket/{id}', [TollController::class, '__invoke'])->name('createTollTicket');
