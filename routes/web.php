6feaf85ffe6bc33c15bb026861c632b25940151d
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-27 21:05:09 +0100

    refactor: Refactor web routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\TollController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/toll', [TollController::class, '__invoke'])->name('listOfTolls');
+Route::get('/', [HomeController::class, '__invoke'])->name('home');
