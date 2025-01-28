fc0e295b2ffc1674f6599861e1d2c37624c685ad
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-28 21:51:45 +0100

    feat: Continuing logic for toll controller

--- app/Http/Controllers/TollController.php
+++ app/Http/Controllers/TollController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TollStation;
+use App\Models\TollStationVehicle;
 use Illuminate\Http\Request;
 
 class TollController extends Controller
@@ -9,8 +11,62 @@ class TollController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke($id)
     {
-        //
+        $toll = $this->getTollFromId($id);
+
+        if (!$toll) {
+            return $this->responseWithRedirect();
+        }
+
+        $vehicles = $this->getVehiclesGroupedById($id);
+        /* $groupedVehicles = $this->getVehiclesGroupBy($tolls, 'id'); */
+
+        return response()->json([
+            'toll' => $toll,
+            'vehicles' => $vehicles
+            /* 'KE' => $KHEEEE */
+        ], 200);
+
+        //return view('showToll');
     }
+
+    private function getTollFromId($id)
+    {
+        return TollStation::find($id);
+    }
+
+    private function getVehiclesGroupedById($id)
+    {
+        return TollStationVehicle::with('vehicle')
+            ->where('toll_station_id', $id)->groupBy('toll_station_id', 'vehicle_id', 'toll_value')
+            ->selectRaw('vehicle_id, toll_value, COUNT(*) as totalCount')
+            ->get();
+    }
+
+    private function responseWithRedirect()
+    {
+        return redirect()->route('home');
+    }
+
+
+    /* private function getVehiclesGroupBy($tolls, $groupField)
+    {
+        $groupedVehiclesWithLength = [];
+
+        foreach ($tolls as $toll) {
+            $groupedVehicles = $toll->vehicles->groupBy($groupField);
+
+            $groupedVehiclesWithLength[] = [
+                'vehicles' => $groupedVehicles,
+                'count' => $groupedVehicles
+            ]
+        }
+
+        return $groupedVehicles;
+    }
+
+    private function makeFinalTolls(){
+
+    } */
 }
