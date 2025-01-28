106536236ddc16ba4fa4096cf9d6cb451737a296
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-28 16:42:05 +0100

    feat: Add footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+<footer class="bg-[#2D2A25]">
+    <div class="w-full mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="/offert" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <span class="self-center text-2xl font-bold whitespace-nowrap text-white">Toll System</span>
+                <div class="flex gap-4">
+                    <a href="www.linkedin.com/in/antonio-guillen-garcia">
+                        <img src="https://skillicons.dev/icons?i=linkedin" />
+                    </a>
+                    <a href="https://github.com/AntonioGuillen123">
+                        <img src="https://skillicons.dev/icons?i=github" />
+                    </a>
+                </div>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium mt-4 text-white sm:mb-0">
+                <li>
+                    <a href="/" class="hover:underline me-4 md:me-6">Home</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-400 sm:mx-auto lg:my-8" />
+        <span class="block text-sm text-white sm:text-center ">© 2025 <a
+                href="https://github.com/AntonioGuillen123" class="underline hover:no-underline">Antonio Guillén García</a>. All rights reserved.</span>
+    </div>
+</footer>
