20a1db0b914e3df05bc6c866393e232c20717c21
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-28 16:38:05 +0100

    feat: Add Header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-</div>
+<header>
+    <div class="h-28 flex flex-col gap-4 font-bold bg-[#2D2A25] justify-center items-center">
+        <a href="/" class="text-2xl md:text-4xl text-white tracking-wide">TOLL SYSTEM</a>
+    </div>
+</header>
