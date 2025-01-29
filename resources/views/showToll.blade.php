8646b3b913a8c0493c3a46e26dbc508d331decbe
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-29 21:40:51 +0100

    style: Add style to toll blade

--- resources/views/showToll.blade.php
+++ resources/views/showToll.blade.php
@@ -1,12 +1,16 @@
 @extends('layout.app')
 
+@section('title')
+    {{ $toll->name }}
+@endsection
+
 @section('content')
-    <div class="flex flex-wrap justify-around p-4 w-full">
-        <div id="headerToll">
-            <div>
+    <div class="flex flex-col flex-wrap gap-16 p-4 w-full">
+        <div id="headerToll" class="flex flex-col gap-8 text-white border-2 border-black rounded-lg bg-[#434141] p-2">
+            <div class="flex justify-center text-3xl font-bold ">
                 {{ $toll->name }}
             </div>
-            <div>
+            <div class="flex text-2xl justify-around">
                 <div>
                     <b>Id:</b> {{ $toll->id }}
                 </div>
@@ -18,7 +22,7 @@
                 </div>
             </div>
         </div>
-        <div id="bodyToll">
+        <div id="bodyToll" class="flex flex-wrap text-white justify-center gap-16">
             @foreach ($vehicles as $vehicle)
                 @php
                     $totalCount = $vehicle->totalCount;
@@ -36,38 +40,42 @@
 
                     $totalCountText = $totalCount > 1 ? $totalCount . ' Times' : $totalCount . ' Time';
                 @endphp
-                <div>
+                <div class="flex flex-col border-2 w-56 h-64 justify-between bg-[#434141] hover:bg-[#5b5858] border-black rounded-lg p-2">
                     <div>
-                        <div>
+                        <div class="flex justify-center font-bold">
                             {{ $tuition }} <b>-</b> {{ $type }}
                         </div>
-                        <div>
-                            <b>Id: </b> {{ $vehicleId }}
+                        <div class="flex justify-between pt-2">
+                            <div class="font-bold">Id: </div>
+                            <div>{{ $vehicleId }}</div>
                         </div>
-                        <div>
-                            <b>Axles: </b> {{ $vehicle->getAxles() }}
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <b>Total Value: </b> {{ $totalValue }} €
-                        </div>
-                        <div>
-                            <b>Ticket Value: </b> {{ $ticketValue }} €
-                        </div>
-                        <div>
-                            <b>Base Value: </b> {{ $baseValue }} €
+                        <div class="flex justify-between">
+                            <div class="font-bold">Axles: </div>
+                            <div>{{ $vehicle->getAxles() }}</div>
                         </div>
                     </div>
                     <div>
-                        <div>
-                            <b>Passed: </b> {{ $totalCountText }}
+                        <div class="flex justify-between">
+                            <div class="font-bold">Total Value: </div>
+                            <div>{{ $totalValue }} €</div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div class="font-bold">Ticket Value: </div>
+                            <div>{{ $ticketValue }} €</div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div class="font-bold">Base Value: </div>
+                            <div>{{ $baseValue }} €</div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="flex justify-between">
+                            <div class="font-bold">Passed: </div>
+                            <div>{{ $totalCountText }}</div>
                         </div>
                     </div>
                 </div>
+            @endforeach
         </div>
-        {{--   --}}
-        @endforeach
-    </div>
     </div>
 @endsection
