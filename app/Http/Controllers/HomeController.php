a5b48042666767881d136d4fbbc7669b97b6c755
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-29 22:26:04 +0100

    feat: Add vehicles on home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\TollStation;
+use App\Models\Vehicle;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,11 +14,16 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         $tolls = $this->getTollsFromDB();
+        $vehicles = $this->getVehiclesFromDB();
 
-        return view('home', compact('tolls'));
+        return view('home', compact('tolls', 'vehicles'));
     }
 
     private function getTollsFromDB(){
         return TollStation::all();
+    }
+
+    private function getVehiclesFromDB(){
+        return Vehicle::all();
     }
 }
