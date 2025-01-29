568a1c330b0f9911fb1c2061877f9d3e5afb9842
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-29 23:08:33 +0100

    feat: Add vehicles to home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="flex flex-wrap justify-around p-4 w-full">
+    <div class="flex flex-wrap gap-5 justify-around p-4 w-full">
         <div id="tolls"
-            class="flex flex-col bg-[#434141] rounded-lg border-2 border-black text-white font-bold w-[17rem] overflow-hidden gap-2">
+            class="flex flex-col bg-[#434141] rounded-lg border-2 border-black text-white font-bold w-80 overflow-hidden gap-2">
             <div class="flex text-2xl justify-center pt-1 px-1">Tolls</div>
             <div class="text-lg">
                 @forelse ($tolls as $toll)
-                    <a class="flex justify-around gap-1 min-w-56 border-t-[1px] p-1.5 cursor-pointer hover:bg-[#5b5858]"
+                    <a class="flex justify-around border-t-[1px] p-1.5 cursor-pointer hover:bg-[#5b5858]"
                         href="{{ route('showToll', $toll->id) }}">
                         <div>
                             {{ $toll->id }}
@@ -26,5 +26,46 @@
                 @endforelse
             </div>
         </div>
-    </div>
+        <div id="vehicles"
+            class="flex flex-col bg-[#434141] rounded-lg border-2 border-black text-white font-bold w-80 overflow-hidden gap-2">
+            <div class="flex text-2xl justify-center pt-1 px-1">Vehicles</div>
+            <div class="text-lg">
+                @forelse ($vehicles as $vehicle)
+                    <a class="flex justify-between items-center border-t-[1px] p-1.5 px-4 cursor-pointer hover:bg-[#5b5858]"
+                        href="{{ route('showVehicle', $vehicle->id) }}">
+                        <div class="flex">
+                            {{ $vehicle->id }}
+                        </div>
+                        <div>
+                            {{ $vehicle->tuition }}
+                        </div>
+                        <div class="flex justify-end font-normal w-12">
+                            {{ $vehicle->vehicleType->type }}
+                        </div>
+                    </a>
+                @empty
+                    <div class="flex justify-center pb-1">
+                        There are no vehicles :(
+                    </div>
+                @endforelse
+            </div>
+        </div>
+       {{--  <div id="vehicles" class="bg-[#434141] rounded-lg border-2 border-black text-white font-bold w-80 overflow-hidden">
+            <div class="text-2xl text-center pt-1 pb-1">Vehicles</div>
+            <div class="text-lg flex flex-col">
+                @forelse ($vehicles as $vehicle)
+                    <a class="flex justify-between items-center border-t-[1px] px-4 py-2 cursor-pointer hover:bg-[#5b5858]"
+                        href="{{ route('showVehicle', $vehicle->id) }}">
+                        <div class="w-1/6 text-center">{{ $vehicle->id }}</div>
+                        <div class="w-2/6 text-center">{{ $vehicle->tuition }}</div>
+                        <div class="w-3/6 text-center font-normal">{{ $vehicle->vehicleType->type }}</div>
+                    </a>
+                @empty
+                    <div class="flex justify-center py-2">
+                        There are no vehicles :(
+                    </div>
+                @endforelse
+            </div>
+        </div>
+    </div> --}}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TollController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, '__invoke'])->name('home');
 
 Route::get('/toll/{id}', [TollController::class, '__invoke'])->name('showToll');
+
+Route::get('/vehicle/{id}', [VehicleController::class, '__invoke'])->name('showVehicle');
