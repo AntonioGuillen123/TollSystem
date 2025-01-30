d572b11c1eef8c449b807a6ccf64e0e476ed2828
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-30 21:31:56 +0100

    test: Test home controller

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\TollStation;
+use App\Models\Vehicle;
+use Database\Seeders\TestSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_CheckIfHomeIsLoadedWithData()
+    {
+        $this->seed(TestSeeder::class);
+
+        $data = [
+            'tolls' => TollStation::all(),
+            'vehicles' => Vehicle::all()
+        ];
+
+        $response = $this->get(route('home'));
+
+        $response
+            ->assertStatus(200)
+            ->assertViewIs('home')
+            ->assertViewHasAll($data);
+    }
+}
