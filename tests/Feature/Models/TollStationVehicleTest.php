fc1c139a83f1f65647353f5d58b5e53ae0211d23
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-01-30 22:35:35 +0100

    test: Test toll station vehicle model :)

---
+++ tests/Feature/Models/TollStationVehicleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\TollStationVehicle;
+use App\Models\TollStation;
+use App\Models\Vehicle;
+use Database\Seeders\DatabaseSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TollStationVehicleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_CheckIfTollStationVehicleHasVehicleRelationship(){
+        $this->seed(DatabaseSeeder::class);
+
+        $tollStationVehicle = TollStationVehicle::find(1);
+        
+        $this->assertInstanceOf(Vehicle::class, $tollStationVehicle->vehicle);
+    }
+
+    public function test_CheckIfTollStationVehicleHasTollStationRelationship(){
+        $this->seed(DatabaseSeeder::class);
+
+        $tollStationVehicle = TollStationVehicle::find(1);
+        
+        $this->assertInstanceOf(TollStation::class, $tollStationVehicle->tollStation);
+    }
+}
